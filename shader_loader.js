84c2d000261dacd3afbb8464cf2021b56896b6f2
Meldexun <27779321+Meldexun@users.noreply.github.com>
2024-11-11 15:53:28 +0100

    Move shader loading into its own file

---
+++ shader_loader.js
@@ -0,0 +1,36 @@
+
+async function loadProgram(gl, vertexShader, fragmentShader) {
+	const program = gl.createProgram();
+	const vsh = await loadShader(gl, gl.VERTEX_SHADER, vertexShader);
+	const fsh = await loadShader(gl, gl.FRAGMENT_SHADER, fragmentShader);
+
+	gl.attachShader(program, vsh);
+	gl.attachShader(program, fsh);
+
+	gl.linkProgram(program);
+	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+		const info = gl.getProgramInfoLog(program);
+		throw `Could not link program (id=${program}). \n\n${info}`;
+	}
+
+	gl.deleteShader(vsh);
+	gl.deleteShader(fsh);
+
+	return program;
+}
+
+async function loadShader(gl, type, source) {
+	const shader = gl.createShader(type);
+	await fetch(source)
+		.then(res => res.text())
+		.then(text => gl.shaderSource(shader, text))
+		.catch(e => console.log(e));
+
+	gl.compileShader(shader);
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		const info = gl.getShaderInfoLog(shader);
+		throw `Could not compile shader (id=${shader}). \n\n${info}`;
+	}
+
+	return shader;
+}
