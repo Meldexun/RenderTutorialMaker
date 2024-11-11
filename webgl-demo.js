e190613fb2c64f0fc47d1f951cebe0d6a9de26c3
Meldexun <27779321+Meldexun@users.noreply.github.com>
2024-11-11 17:16:20 +0100

    Draw cube instead of triangle

--- webgl-demo.js
+++ webgl-demo.js
@@ -33,13 +33,50 @@ async function init(gl) {
 
 	const vbo = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-	const data = new Float32Array([
-		0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0,
-		1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0,
-		0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0
-	]);
-	gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+		1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+		0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+		0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+		1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+
+		0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0,
+		1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0,
+		1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0,
+		0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0,
+
+		0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0,
+		0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0,
+		0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0,
+
+		1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0,
+		1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0,
+		1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0,
+
+		0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0,
+		1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0,
+		1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0,
+		0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0,
+
+		0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0,
+		1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0,
+		1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0,
+		0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0
+	]), gl.STATIC_DRAW);
 	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+	const ebo = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array([
+		 0,  1,  2,  2,  3,  0,
+		 4,  5,  6,  6,  7,  4,
+		 8,  9, 10, 10, 11,  8,
+		12, 13, 14, 14, 15, 12,
+		16, 17, 18, 18, 19, 16,
+		20, 21, 22, 22, 23, 20
+	]), gl.STATIC_DRAW);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
 	vao = gl.createVertexArray();
 	gl.bindVertexArray(vao);
@@ -48,6 +85,7 @@ async function init(gl) {
 	gl.enableVertexAttribArray(1);
 	gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 28, 0);
 	gl.vertexAttribPointer(1, 4, gl.FLOAT, false, 28, 12);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo);
 	gl.bindBuffer(gl.ARRAY_BUFFER, null);
 	gl.bindVertexArray(null);
 }
@@ -70,7 +108,7 @@ function loop(time) {
 	gl.uniformMatrix4fv(u_ModelMatrix, false, model.toFloat32Array());
 
 	gl.bindVertexArray(vao);
-	gl.drawArrays(gl.TRIANGLES, 0, 3);
+	gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_INT, 0);
 	gl.bindVertexArray(null);
 
 	gl.useProgram(null);
