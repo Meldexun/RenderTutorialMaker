f0d1f26eba0f1964d147f20c96bdbf08c739a210
Meldexun <27779321+Meldexun@users.noreply.github.com>
2024-11-13 00:44:58 +0100

    Visualize projection matrix

--- third_person.js
+++ third_person.js
@@ -3,6 +3,7 @@ let u_ProjectionMatrixTP;
 let u_ViewMatrixTP;
 let u_ModelMatrixTP;
 let vaoTP;
+let vao1TP;
 
 async function initTP(gl) {
 	programTP = await loadProgram(gl, "test.vsh", "test.fsh");
@@ -67,6 +68,58 @@ async function initTP(gl) {
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo);
 	gl.bindBuffer(gl.ARRAY_BUFFER, null);
 	gl.bindVertexArray(null);
+
+	const vbo1 = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vbo1);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+		-1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+		 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+		 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+		 1.0,  1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+		 1.0,  1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+		-1.0,  1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+		-1.0,  1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+		-1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+	
+		-1.0, -1.0,  1.0, 1.0, 1.0, 1.0, 1.0,
+		 1.0, -1.0,  1.0, 1.0, 1.0, 1.0, 1.0,
+		 1.0, -1.0,  1.0, 1.0, 1.0, 1.0, 1.0,
+		 1.0,  1.0,  1.0, 1.0, 1.0, 1.0, 1.0,
+		 1.0,  1.0,  1.0, 1.0, 1.0, 1.0, 1.0,
+		-1.0,  1.0,  1.0, 1.0, 1.0, 1.0, 1.0,
+		-1.0,  1.0,  1.0, 1.0, 1.0, 1.0, 1.0,
+		-1.0, -1.0,  1.0, 1.0, 1.0, 1.0, 1.0,
+	
+		-1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+		-1.0, -1.0,  1.0, 1.0, 1.0, 1.0, 1.0,
+		 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+		 1.0, -1.0,  1.0, 1.0, 1.0, 1.0, 1.0,
+		 1.0,  1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+		 1.0,  1.0,  1.0, 1.0, 1.0, 1.0, 1.0,
+		-1.0,  1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+		-1.0,  1.0,  1.0, 1.0, 1.0, 1.0, 1.0
+	]), gl.STATIC_DRAW);
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+	const ebo1 = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo1);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array([
+		 0,  1,  2,  3,  4,  5,  6,  7,
+		 8,  9, 10, 11, 12, 13, 14, 15,
+		16, 17, 18, 19, 20, 21, 22, 23
+	]), gl.STATIC_DRAW);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+	vao1TP = gl.createVertexArray();
+	gl.bindVertexArray(vao1TP);
+	gl.bindBuffer(gl.ARRAY_BUFFER, vbo1);
+	gl.enableVertexAttribArray(0);
+	gl.enableVertexAttribArray(1);
+	gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 28, 0);
+	gl.vertexAttribPointer(1, 4, gl.FLOAT, false, 28, 12);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo1);
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+	gl.bindVertexArray(null);
 }
 
 function renderTP(gl, time) {
@@ -76,6 +129,7 @@ function renderTP(gl, time) {
 	gl.enable(gl.DEPTH_TEST);
 
 	gl.useProgram(programTP);
+
 	let projection = new DOMMatrix();
 	let view = new DOMMatrix();
 	let model = new DOMMatrix();
@@ -90,6 +144,15 @@ function renderTP(gl, time) {
 
 	gl.bindVertexArray(vaoTP);
 	gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_INT, 0);
+	gl.bindVertexArray(null);
+
+	model = perspectiveTP(60.0, 16.0 / 9.0, 0.5, 3.0);
+	model = model.translateSelf(0.0, 0.0, -2.0);
+	model = model.inverse();
+	gl.uniformMatrix4fv(u_ModelMatrixTP, false, model.toFloat32Array());
+
+	gl.bindVertexArray(vao1TP);
+	gl.drawElements(gl.LINES, 24, gl.UNSIGNED_INT, 0);
 	gl.bindVertexArray(null);
 
 	gl.useProgram(null);
