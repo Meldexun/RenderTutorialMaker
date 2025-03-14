fa7731ffe57a6bccc24f87f4c8ff69c0e147d236
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-03-14 16:23:15 +0100

    Add basic backend for tutorial creation requests

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
 const readline = require('readline');
 const http_terminator = require('http-terminator');
 
@@ -9,6 +11,44 @@ const port = process.argv.length >= 4 ? process.argv[3] : "25565";
 console.log("Server starting...");
 const app = express();
 app.use(express.static(folder));
+app.post('/api/endpoint', multer().none(), (req, res) => {
+	const { body } = req;
+	console.log({ body });
+	res.send('Data received successfully');
+	
+	const tutorialDir = folder + "/tutorials/" + body.name;
+	fs.mkdir(tutorialDir, (err, dir) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		const config = {
+			title: body.name,
+			views: [],
+			properties: []
+		};
+		for (let i = 0; body["view_" + i + "_name"]; i++) {
+			config.views[i] = {
+				id: body["view_" + i + "_name"],
+				width: body["view_" + i + "_width"],
+				height: body["view_" + i + "_height"]
+			};
+			fs.writeFileSync(tutorialDir + "/" + body["view_" + i + "_name"] + "_init.js", "(async function(gl) {\n" + body["view_" + i + "_init"] + "});\n", "utf8");
+			fs.writeFileSync(tutorialDir + "/" + body["view_" + i + "_name"] + "_loop.js", "(function(gl, time, properties) {\n" + body["view_" + i + "_loop"] + "});\n", "utf8");
+		}
+		for (let i = 0; body["property_" + i + "_name"]; i++) {
+			config.properties[i] = {
+				id: body["property_" + i + "_name"],
+				width: body["property_" + i + "_type"],
+				height: body["property_" + i + "_defaultValue"]
+			};
+		}
+		fs.writeFileSync(tutorialDir + "/config.json", JSON.stringify(config), "utf8");
+	});
+	
+	console.log('Tutorial created successfully');
+});
 const server = app.listen(port);
 console.log("Server started");
 
