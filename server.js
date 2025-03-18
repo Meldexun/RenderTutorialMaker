76db8335e21851874d08569a6260318f2b35df5e
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-03-18 17:50:14 +0100

    Update tutorial save request handling
    - Support for all property types (with their respective attributes)
    - Error handling

--- server.js
+++ server.js
@@ -16,16 +16,19 @@ app.use(express.static(folder));
 app.post('/api/endpoint', multer().none(), (req, res) => {
 	const { body } = req;
 	console.log({ body });
-	res.send('Data received successfully');
-	
+
 	// save tutorial
-	const tutorialDir = folder + "/tutorials/" + body.name;
-	fs.mkdir(tutorialDir, (err, dir) => {
-		if (err) {
-			console.log(err);
-			return;
+	const temp = folder + "/temp/" + body.name;
+	try {
+		// delete existing
+		if (fs.existsSync(temp)) {
+			fs.rmSync(temp, { recursive: true });
 		}
 
+		// create directory
+		fs.mkdirSync(temp, { recursive: true });
+
+		// save tutorial files into temp folder
 		const config = {
 			title: body.name,
 			views: [],
@@ -37,20 +40,81 @@ app.post('/api/endpoint', multer().none(), (req, res) => {
 				width: body["view_" + i + "_width"],
 				height: body["view_" + i + "_height"]
 			};
-			fs.writeFileSync(tutorialDir + "/" + body["view_" + i + "_name"] + "_init.js", "(async function(gl) {\n" + body["view_" + i + "_init"] + "});\n", "utf8");
-			fs.writeFileSync(tutorialDir + "/" + body["view_" + i + "_name"] + "_loop.js", "(function(gl, time, properties) {\n" + body["view_" + i + "_loop"] + "});\n", "utf8");
+			fs.writeFileSync(temp + "/" + body["view_" + i + "_name"] + "_init.js", "(async function(gl) {\n" + body["view_" + i + "_init"] + "\n});\n", "utf8");
+			fs.writeFileSync(temp + "/" + body["view_" + i + "_name"] + "_loop.js", "(function(gl, time, properties) {\n" + body["view_" + i + "_loop"] + "\n});\n", "utf8");
 		}
 		for (let i = 0; body["property_" + i + "_name"]; i++) {
 			config.properties[i] = {
 				id: body["property_" + i + "_name"],
-				width: body["property_" + i + "_type"],
-				height: body["property_" + i + "_defaultValue"]
+				type: body["property_" + i + "_type"],
+				defaultValue: body["property_" + i + "_defaultValue"]
 			};
+			switch (body["property_" + i + "_type"]) {
+				case "boolean":
+					if (body["property_" + i + "_input_checkBox"] === "on") {
+						config.properties[i]["input_checkBox"] = true;
+					}
+					break;
+				case "number":
+				case "vec2":
+				case "vec3":
+				case "vec4":
+				case "mat2":
+				case "mat3":
+				case "mat4":
+					if (body["property_" + i + "_min"] && body["property_" + i + "_min"] !== "") {
+						config.properties[i]["min"] = body["property_" + i + "_min"];
+					}
+					if (body["property_" + i + "_max"] && body["property_" + i + "_max"] !== "") {
+						config.properties[i]["max"] = body["property_" + i + "_max"];
+					}
+					if (body["property_" + i + "_input_number"] === "on") {
+						config.properties[i]["input_number"] = true;
+					}
+					if (body["property_" + i + "_type"] === "number" && body["property_" + i + "_input_slider"] === "on") {
+						config.properties[i]["input_slider"] = true;
+					}
+					break;
+				case "string (single-line)":
+				case "string (multi-line)":
+					config.properties[i]["language"] = body["property_" + i + "_language"];
+					if (body["property_" + i + "_type"] === "string (single-line)" && body["property_" + i + "_input_text"] === "on") {
+						config.properties[i]["input_text"] = true;
+					}
+					if (body["property_" + i + "_type"] === "string (multi-line)" && body["property_" + i + "_input_textarea"] === "on") {
+						config.properties[i]["input_textarea"] = true;
+					}
+					break;
+				default:
+					throw new Error("Invalid property type '" + body["property_" + i + "_type"] + "'");
+			}
+			if (body["property_" + i + "_input_presets"] === "on") {
+				config.properties[i]["input_presets"] = true;
+				config.properties[i]["presets"] = [];
+				for (let j = 0; body["property_" + i + "_preset_" + j]; j++) {
+					config.properties[i]["presets"][j] = body["property_" + i + "_preset_" + j];
+				}
+			}
 		}
-		fs.writeFileSync(tutorialDir + "/config.json", JSON.stringify(config), "utf8");
-	});
-	
-	console.log('Tutorial "' + body.name + '" saved successfully!');
+		fs.writeFileSync(temp + "/config.json", JSON.stringify(config, null, 2), "utf8");
+
+		// everything saved successfully into temp folder -> move into tutorial folder
+		const target = folder + "/tutorials/" + body.name;
+		if (fs.existsSync(target)) {
+			fs.rmSync(target, { recursive: true });
+		}
+		fs.renameSync(temp, target);
+
+		res.send('Tutorial saved successfully!');
+	} catch (err) {
+		// delete temp files
+		if (fs.existsSync(temp)) {
+			fs.rmSync(temp, { recursive: true });
+		}
+
+		console.log(err);
+		res.send('Failed saving tutorial!');
+	}
 });
 // start server listening on given port
 const server = app.listen(port);
