06d0423c1287a71cffa077f66ec5fdc4e1ff4e7d
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-03-20 03:21:34 +0100

    Delete unused files

--- src/util/obj_loader.js
+++
@@ -1,7 +0,0 @@
-const OBJ = require('webgl-obj-loader');
-
-export async function loadOBJ(gl, source) {
-	var mesh = new OBJ.Mesh(await (fetch(source).then(res => res.text())));
-	OBJ.initMeshBuffers(gl, mesh);
-	return mesh;
-}
