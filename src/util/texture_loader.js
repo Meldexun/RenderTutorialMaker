d58eadcd075ca4f2cc09b0749b167ea7faef29db
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-03-20 03:22:22 +0100

    Remove unnecessary async modifier

--- src/util/texture_loader.js
+++ src/util/texture_loader.js
@@ -1,5 +1,5 @@
 
-export async function loadTexture(gl, source) {
+export function loadTexture(gl, source) {
 	const texture = gl.createTexture();
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255]));
